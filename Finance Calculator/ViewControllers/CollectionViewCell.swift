12eabb5604ce477da08d1c5c7bb48c1bcdfc3f1a
Kannan Kirishikesan <kannan@wso2.com>
2023-08-26 22:03:49 +0530

    UI Changes

--- Finance Calculator/ViewControllers/CollectionViewCell.swift
+++ Finance Calculator/ViewControllers/CollectionViewCell.swift
@@ -18,6 +18,7 @@ class CollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var durationLabel: UILabel!
     
     var url : String = ""
+
     
     @IBAction func viewButtonClicked(_ sender: UIButton) {
         if let link = URL(string: self.url){
@@ -27,12 +28,36 @@ class CollectionViewCell: UICollectionViewCell {
     }
     
     func configure(with learningMaterial: LearningMaterial){
-        yearLabel.text = "Year - \(learningMaterial.year)"
-        marksLabel.text = "Marks - \(learningMaterial.marks)"
-        dateLabel.text = "Date - \(learningMaterial.date)"
-        durationLabel.text = "Duration - \(learningMaterial.duration)"
+        let boldAttributes: [NSAttributedString.Key: Any] = [
+                .font: UIFont.boldSystemFont(ofSize: yearLabel.font.pointSize)
+        ]
+        
+        let normalAttributes: [NSAttributedString.Key: Any] = [
+            .font: yearLabel.font // Use the same font as the label's default font
+        ]
+        
+        let yearLabelText = "Year - \(learningMaterial.year)"
+        let marksLabelText = "Marks - \(learningMaterial.marks)%"
+        let dateLabelText = "Date - \(learningMaterial.date)"
+        let durationLabelText = "Duration - \(learningMaterial.duration) Hours"
+        
+        let yearAttributedString = NSMutableAttributedString(string: yearLabelText, attributes: boldAttributes)
+        let marksAttributedString = NSMutableAttributedString(string: marksLabelText, attributes: boldAttributes)
+        let dateAttributedString = NSMutableAttributedString(string: dateLabelText, attributes: boldAttributes)
+        let durationAttributedString = NSMutableAttributedString(string: durationLabelText, attributes: boldAttributes)
+        
+        yearAttributedString.addAttributes(normalAttributes, range: NSRange(location: 6, length: yearLabelText.count - 6))
+        marksAttributedString.addAttributes(normalAttributes, range: NSRange(location: 7, length: marksLabelText.count - 7))
+        dateAttributedString.addAttributes(normalAttributes, range: NSRange(location: 6, length: dateLabelText.count - 6))
+        durationAttributedString.addAttributes(normalAttributes, range: NSRange(location: 10, length: durationLabelText.count - 10))
+        
+        yearLabel.attributedText = yearAttributedString
+        marksLabel.attributedText = marksAttributedString
+        dateLabel.attributedText = dateAttributedString
+        durationLabel.attributedText = durationAttributedString
+        
         url = learningMaterial.url
     }
-    
+
 }
 
